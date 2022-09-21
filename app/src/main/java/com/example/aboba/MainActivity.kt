724f8e348d8bea30a7fa2f2d5f6758fb7e2834e2
Lnight <94696816+EvgenyPlaksin@users.noreply.github.com>
2022-09-21 23:20:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/aboba/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aboba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.namespace.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
